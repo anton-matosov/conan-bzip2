694e1dba009036f28d4ab3acfcfbdbd9fe12ac48
Anton Matosov <anton.matosov@gmail.com>
2017-07-26 21:24:19 -0700

    Fix filtering builds and loosing build_requires and env

--- build.py
+++ build.py
@@ -6,11 +6,11 @@ if __name__ == "__main__":
     builder.add_common_builds(shared_option_name="bzip2:shared", pure_c=True)
     if platform.system() == "Windows": # Library not prepared to create a .lib to link with (only dll)
         # Remove shared builds in Windows
-        static_builds = []
-        for build in builder.builds:
-            if not build[1]["bzip2:shared"]:
-                static_builds.append([build[0], build[1]])
-            
-        builder.builds = static_builds
+        filtered_builds = []
+        for settings, options, env_vars, build_requires in builder.builds:
+            if not options["bzip2:shared"]:
+                filtered_builds.append([settings, options, env_vars, build_requires])
+        builder.builds = filtered_builds
+
     builder.run()
 
